3e7d9110298d9566449732d2484af924f1f2d5bb
Nwanochie Emmanuel <emmanuel.nwanochie.197850@unn.edu.ng>
2020-10-26 11:00:31 +0100

    complete desktop views of home, about, activities, contact us pages

--- app.js
+++ app.js
@@ -18,7 +18,13 @@ app.get("/", function (req, res) {
 app.get("/about", function (req, res) {
   res.sendFile(__dirname + "/about.html");
 });
-
+app.get("/activities", function (req, res) {
+    res.sendFile(__dirname + "/activities.html");
+  });
+  
+app.get("/contact", function (req, res) {
+    res.sendFile(__dirname + "/contact.html");
+  });
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
