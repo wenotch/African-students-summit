1794e741e24fcf2eaae4a572489a9035ed2af964
Nwanochie Emmanuel <emmanuel.nwanochie.197850@unn.edu.ng>
2020-10-27 23:13:31 +0100

    reverted

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/about", function (req, res) {
+  res.sendFile(__dirname + "/about.html");
+});
+app.get("/activities", function (req, res) {
+    res.sendFile(__dirname + "/activities.html");
+  });
+  
+app.get("/contact", function (req, res) {
+    res.sendFile(__dirname + "/contact.html");
+  });
+app.get("/join", function (req, res) {
+    res.sendFile(__dirname + "/join.html");
+  });  
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server started on port 3000");
+});
